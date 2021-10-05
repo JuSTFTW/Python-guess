14fd8e2a822853e92c96b9c380fb4dad804e64b1
Justin <playstoretw0612@gmail.com>
2021-10-05 02:00:05 -0500

    final

--- guess-game.py
+++ guess-game.py
@@ -4,14 +4,26 @@ x = random.randint(1,50)
 print(x)
 
 for i in range(5):
-    y = int(input("Enter an integer between 1~50: "))
-    if x ==y:
-        print("You win!")
+    bingo = False
+    while True:
+        try:
+            y = int(input("Enter an integer between 1~50: "))
+            if x ==y:
+                print("You win!")
+                bingo = True
+                break
+            if x>y:
+                print("Too low, guess again.")
+            else: 
+                print("Too high, guess again.")
+            break
+        except:
+            print("Please enter a valid number.")
+    if bingo:
         break
-    if x>y:
-        print("Too low, guess again.")
-    else: 
-        print("Too high, guess again.")
 
-print(f"Answer is {x}")
+if bingo:
+    print("Congrats!")
+else:
+    print(f"Answer is {x}")
     
